0f1c00eb39ee8234255d5f47941e340c6404ed71
Alan Raff <alanr917@hotmail.com>
2021-08-20 07:26:53 -0400

    display URLs in styled table

--- web/php/src/links.php
+++ web/php/src/links.php
@@ -1,20 +1,52 @@
-<?php
-$host = 'db';
-$user = 'MYSQL_USER';
-$pass = 'MYSQL_PASSWORD';
-$database = 'MYSQL_DATABASE';
-$conn = new mysqli($host, $user, $pass, $database);
-$sql = 'SELECT * FROM links';
+<!DOCTYPE html>
+<head>
+    <style type="text/css">
+    table.blueTable {
+      width: 100%;
+      text-align: center;
+      font-size: 48px;
+  }
+  table.blueTable td, table.blueTable th {
+    border: 1px solid #000000;
+  }
+  table.blueTable thead {
+      background: #0B6FA4;
+  }
+  table.blueTable thead th {
+      font-weight: bold;
+      color: #FFFFFF;
+      text-align: center;
+  }
+</style>
+</head>
+<body>
+    <center>
+        <table class="blueTable">
+            <thead>
+                <tr>
+                    <th>Credential Dump URLs</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $host = 'db';
+                $user = 'MYSQL_USER';
+                $pass = 'MYSQL_PASSWORD';
+                $database = 'MYSQL_DATABASE';
+                $conn = new mysqli($host, $user, $pass, $database);
+                $sql = 'SELECT * FROM links';
 
-if ($result = $conn->query($sql)) {
-    while ($data = $result->fetch_object()) {
-        $links[] = $data;
-    }
-}
+                if ($result = $conn->query($sql)) {
+                    while ($data = $result->fetch_object()) {
+                        $links[] = $data;
+                    }
+                }
 
-foreach ($links as $link) {
-    echo "<br>";
-    echo $link->url;
-    echo "<br>";
-}
-?>
+                foreach ($links as $link) {
+                    echo "<tr><td><a href='" . $link->url . "'>" . $link->url . "</a></td><tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </center>
+</body>
